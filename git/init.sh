2a4b499f26e56d1cf18f2b085eb054110f7097f8
Marc-Antoine <me@maferland.com>
2022-07-25 23:02:16 -0400

    add git init command

--- git/init.sh
+++ git/init.sh
@@ -10,3 +10,4 @@ git config --global user.name Marc-Antoine Ferland
 git config --global user.email "me@maferland.com"
 git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
 git config --global interactive.diffFilter "diff-so-fancy --patch"
+git config --global --add --bool push.autoSetupRemote true
